14350b805000e306757755f0df8d23186e5a37bc
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-11-26 15:21:50 +0900

    practice1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student{
+	int ID;
+	char name[10];
+	double greade;
+	
+};
+
+int main(int argc, char *argv[]) {
+	
+	struct student s1={24,"MinJi",4.3
+	};
+	
+	return 0;
+}
